d2bb5bf05ca6cc66e1a8828e6d961360df5b6ca9
deepak <deepaksai678@gmail.com>
2023-10-24 12:24:28 +0530

    loops0

---
+++ ifloop.py
@@ -0,0 +1,5 @@
+k=5
+n=1
+if k > n:
+    print("k is larger",k)
+
---
+++ test.py
@@ -0,0 +1,41 @@
+"""
+x=4
+print("value of X",x)
+x="hello"
+print("string of x", x)
+
+a=str(3)
+print(type(a)) # string
+print(a) 
+b=int(3)
+print(type(b)) # integer
+print(b)
+c=float(3)
+print(type(c)) # float
+print(c) # 3
+
+
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+"""
+
+"""
+x = "python is programming language"
+print(x)
+
+a = "python"
+b = "is programming"
+c = "language"
+
+print(a,b,c)
+print(a+b+c)
+d = a+b+c
+print(type(d))
+"""
+e=5
+f=10
+print(e+f)
